a42f3fd75be485131b044e92abc5b7de9c4e1500
RoseLu <qifei.lu1994@gmail.com>
2017-02-10 16:53:52 -0800

    score and comments

--- Assignment2.java
+++ Assignment2.java
@@ -1,3 +1,7 @@
+/*
+score: 9 + 1 (extra credit)
+comments: perfect work!
+*/
 /**
  * Created by Rose on 1/21/17.
  * Assignment for your lecture 2. Please finish all the questions under
@@ -49,7 +53,7 @@ public class Assignment2 {
      * If the salary is less than or equal to 8900, the Social Security Tax is 6.2% of the salary.
      * If the salary is more than 8900, the Social Security Tax is 6.2% of 106,800.
      */
-    public double socialSecurityTax(Employee employee) {
+    public double socialSecurityTax(Employee employee) {                                  //correct
         //write your code here
     	double sst = 0;
     	if(employee.salary <= 8900){
@@ -67,7 +71,7 @@ public class Assignment2 {
      * If the employee is under 35, rate is 3% of salary; if the employee is between 35 and 50(inclusive), rate is 4% of salary;
      * If the employee is between 50 and 60(exclusive), rate is 5% of salary; If the employee is above 60, rate is 6% of salary.
      */
-    public double insuranceCoverage(Employee employee) {
+    public double insuranceCoverage(Employee employee) {                       //correct
         //write your code here
     	double rate = 0;
     	if(employee.age < 35) {
@@ -88,7 +92,7 @@ public class Assignment2 {
      * For example, Alice's salary is 1000, John's salary is 500, Jenny's salary is 1200, you should print:
      * John Alice Jenny
      */
-    public void sortSalary(Employee e1, Employee e2, Employee e3) {
+    public void sortSalary(Employee e1, Employee e2, Employee e3) {                    //correct
         //write your code here 
     	Employee[] employees = {e1,e2,e3};
 		Employee tmp;
@@ -113,7 +117,7 @@ public class Assignment2 {
      * Do not change the input of this method.
      * Try to add a new method in Employee class: public void raiseSalary(double byPercent)
      */
-    public void tripleSalary(Employee employee) {
+    public void tripleSalary(Employee employee) {                  //correct
         //write your code here
     	employee.salary = 3 * employee.salary; 	
     }
@@ -123,7 +127,7 @@ public class Assignment2 {
     /**
      * Write a method to determine whether a number is prime
      */
-    public boolean isPrime(int n) {
+    public boolean isPrime(int n) {                                 //miss one corner case: if n is 2, your return is false, but 2 is a prime number
         //write your code here
     	if (n%2 == 0)
     		return false;
@@ -139,7 +143,7 @@ public class Assignment2 {
      * result has only one digit. For example: Given n = 38, the process is
      * like: 3 + 8 = 11, 1 + 1 = 2. Since 2 has only one digit, return it.
      */
-    public int addDigits(int n) {
+    public int addDigits(int n) {                                      //smart solution!
         //write your code here
     	return (n - 1)%9 + 1;
     }
@@ -151,7 +155,7 @@ public class Assignment2 {
      * another prime factor 7. Note that 1 is typically treated as an ugly
      * number.
      */
-    public boolean isUgly(int n) {
+    public boolean isUgly(int n) {                          //correct
         //write your code here
     	if(n == 0)
     		return false;
@@ -185,6 +189,7 @@ public class Assignment2 {
      * Write your understanding of the reason and explain it.
      */
     /*
+    //correct
      write your understanding here.
      In java, there is only one type of passing and it is value. x and y are only the copy of a and b, 
      so we should write "System.out.println("After: x=" + x.getName()); " and "System.out.println("After: y=" + y.getName());" in the "swap" method
